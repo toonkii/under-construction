912f06e6b7382edaa0e3b606f61df8c35d430da5
Thomas Clarke <thomas.p.clarke@gmail.com>
2026-05-03 11:26:36 -0400

    Set title.

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -6,7 +6,7 @@ export default define.page(function App({ Component }) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>DaisyUI Fresh2</title>
+        <title>Sidereak 365</title>
       </head>
       <body class="container mx-auto h-screen">
         <Component />
